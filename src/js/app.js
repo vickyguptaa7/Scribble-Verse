a43f8ad91761253be4e5b588bc071b0390fe4cce
Vicky Gupta <vickyguptaa7@gmail.com>
2022-05-05 19:03:10 +0530

    local storage and canvas to img

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,15 @@ const body = document.body;
 const pen = document.querySelector('.pen');
 const penColor = document.querySelector('.pen-color');
 const penWidth = document.querySelector('.pen-slider');
-const bucketSet = document.querySelector('.bucket-set');
 const bucketColor = document.querySelector('.bucket-color');
 const eraser = document.querySelector('.eraser');
-const clear = document.querySelector('.clear');
-const saveStorage = document.querySelector('.save-storage');
-const loadStorage = document.querySelector('.load-storage');
-const deleteStorage = document.querySelector('.deleteStorage');
+const clearCanvas = document.querySelector('.clear');
 const saveImage = document.querySelector('.save-image');
 const allTools = document.querySelectorAll('.header-icons-styles');
 const selectedTool = document.querySelector('.selected-tools');
+const saveStorage = document.querySelector('.save-storage');
+const loadStorage = document.querySelector('.load-storage');
+const deleteStorage = document.querySelector('.delete-storage');
 
 const canvas = document.createElement('canvas');
 canvas.id = 'canvas';
@@ -23,10 +22,12 @@ let currPenColor = '#A51DAB';
 let isEraser = false;
 let isMouseDown = false;
 let drawnArray = [];
+let currentDrawState = {};
 // Setting background color
 bucketColor.addEventListener('input', () => {
     currBucketColor = bucketColor.firstElementChild.value;
     createCanvas();
+    restoreCanvas();
 })
 
 // Setting pen color
@@ -79,7 +80,7 @@ function switchToPen() {
 // Create Canvas
 function createCanvas() {
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight-75;
+    canvas.height = window.innerHeight - 75;
     context.fillStyle = currBucketColor;
     context.fillRect(0, 0, canvas.width, canvas.height);
     body.appendChild(canvas);
@@ -88,6 +89,42 @@ function createCanvas() {
 
 createCanvas();
 
+// Draw What is Stored in drawn array
+function restoreCanvas() {
+    for (let i = 0; i < drawnArray.length - 1; i++) {
+        context.beginPath();
+        context.moveTo(drawnArray[i].x, drawnArray[i].y);
+        context.lineWidth = drawnArray[i].size;
+        context.lineCap = 'round';
+        if (drawnArray[i].erase) {
+            context.strokeStyle = bucketColor;
+        } else {
+            context.strokeStyle = drawnArray[i].color;
+        }
+        context.lineTo(drawnArray[i + 1].x, drawnArray[i + 1].y);
+        context.stroke();
+    }
+}
+
+function storeDrawn(x, y, size, color, erase) {
+    const line = {
+        x, y, size, color, erase,
+    };
+    // console.log(line);
+    drawnArray.push(line);
+}
+
+
+// Clear Canvas
+clearCanvas.addEventListener('click', () => {
+    createCanvas();
+    drawnArray = [];
+    selectedTool.textContent = 'Canvas Cleared';
+    setTimeout(switchToPen, 1500);
+});
+
+
+// Mouse Click
 // Get Mouse Position
 function getMousePosition(event) {
     const boundaries = canvas.getBoundingClientRect();
@@ -96,17 +133,6 @@ function getMousePosition(event) {
         y: event.clientY - boundaries.top,
     };
 }
-
-
-function getTouchPosition(event) {
-    const boundaries = canvas.getBoundingClientRect();
-    console.log(event.touches);
-    return {
-        x: event.touches[0].clientX - boundaries.left,
-        y: event.touches[0].clientY - boundaries.top,
-    };
-}
-
 
 canvas.addEventListener('mousedown', (event) => {
     isMouseDown = true;
@@ -123,6 +149,16 @@ canvas.addEventListener('mousemove', (event) => {
         const currentPosition = getMousePosition(event);
         context.lineTo(currentPosition.x, currentPosition.y);
         context.stroke();
+        storeDrawn(
+            currentPosition.x,
+            currentPosition.y,
+            currentPenSize,
+            currPenColor,
+            isEraser
+        );
+    }
+    else {
+        storeDrawn(undefined);
     }
 })
 
@@ -130,6 +166,18 @@ canvas.addEventListener('mouseup', (event) => {
     isMouseDown = false;
 
 })
+
+
+// Touch 
+// Get Touch Position
+function getTouchPosition(event) {
+    const boundaries = canvas.getBoundingClientRect();
+    // console.log(event.touches);
+    return {
+        x: event.touches[0].clientX - boundaries.left,
+        y: event.touches[0].clientY - boundaries.top,
+    };
+}
 
 canvas.addEventListener('touchstart', (event) => {
     isMouseDown = true;
@@ -139,7 +187,7 @@ canvas.addEventListener('touchstart', (event) => {
     context.lineWidth = currentPenSize;
     context.lineCap = 'round';
     context.strokeStyle = currPenColor;
-    console.log(currentPosition);
+    // console.log(currentPosition);
 })
 
 canvas.addEventListener('touchmove', (event) => {
@@ -147,10 +195,61 @@ canvas.addEventListener('touchmove', (event) => {
         const currentPosition = getTouchPosition(event);
         context.lineTo(currentPosition.x, currentPosition.y);
         context.stroke();
+        storeDrawn(
+            currentPosition.x,
+            currentPosition.y,
+            currentPenSize,
+            currPenColor,
+            isEraser
+        );
+    }
+    else {
+        storeDrawn(undefined);
     }
 })
 
 canvas.addEventListener('touchend', (event) => {
     isMouseDown = false;
 
+})
+
+
+
+// Local Storage
+// save to local storage
+saveStorage.addEventListener('click', () => {
+    localStorage.setItem('saveCanvas', JSON.stringify(drawnArray));
+    selectedTool.textContent = 'Canvas Saved';
+    setTimeout(switchToPen, 1500);
+})
+
+// load from local storage
+loadStorage.addEventListener('click', () => {
+    if (localStorage.getItem('saveCanvas')) {
+        drawnArray = JSON.parse(localStorage.saveCanvas);
+        selectedTool.textContent = 'Canvas Loaded';
+        restoreCanvas();
+        setTimeout(switchToPen, 1500);
+    }
+    else {
+        selectedTool.textContent = 'Canvas Not Found';
+        setTimeout(switchToPen, 1500);
+    }
+
+})
+
+// clear local storage
+deleteStorage.addEventListener('click', () => {
+    localStorage.removeItem('saveCanvas');
+    selectedTool.textContent = 'Cleared Local Storage';
+    setTimeout(switchToPen, 1500);
+})
+
+
+// Save Canvas As Image
+saveImage.addEventListener('click', () => {
+    saveImage.firstElementChild.href = canvas.toDataURL('image/jpeg', 1);
+    saveImage.firstElementChild.download = 'paint-example.jpeg';
+    selectedTool.textContent = 'Image File Saved';
+    setTimeout(switchToPen, 1500);
 })
