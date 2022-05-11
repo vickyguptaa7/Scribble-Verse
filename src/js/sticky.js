07db9bbdcba91866e370e5fa8e14ef7278db0f6c
Vicky Gupta <vickyguptaa7@gmail.com>
2022-05-11 23:47:17 +0530

    fix for smartphone touch

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -3,6 +3,9 @@ const stickyWrappers = document.querySelectorAll('.sticky-wrapper');
 
 let posX = 0, posY = 0;
 let isMouseDown = false;
+let isTouched=false;
+
+// for pc to move sticky notes
 stickyWrappers.forEach(stickyWrapper => {
     stickyWrapper.addEventListener('mousedown', (event) => {
         posX = event.clientX - stickyWrapper.offsetLeft;
@@ -17,7 +20,7 @@ stickyWrappers.forEach(stickyWrapper => {
         let moveY = event.clientY - posY;
         let moveX = event.clientX - posX;
 
-        console.log(moveX, moveY);
+        // console.log(moveX, moveY);
         stickyWrapper.style.top = moveY + 'px';
         stickyWrapper.style.left = moveX + 'px';
     });
@@ -35,24 +38,36 @@ stickyWrappers.forEach(stickyWrapper => {
     stickyWrapper.addEventListener('drag', () => false)
 });
 
+// For smartphone to move sticky note
+stickyWrappers.forEach(stickyWrapper => {
+    stickyWrapper.addEventListener('touchstart', (event) => {
+        posX = event.changedTouches[0].clientX - stickyWrapper.offsetLeft;
+        posY = event.changedTouches[0].clientY - stickyWrapper.offsetTop;
+        // console.log(posX,posY);
+        // stickyWrapper.style.top = moveY + 'px';
+        // stickyWrapper.style.left = moveX + 'px';
+        isTouched = true;
+    })
+    stickyWrapper.addEventListener('touchmove', (event) => {
+        if (!isTouched) return;
 
-// function isOutOfBound(stickyWrapper) {
-//     let containerInfo = stickyContainer.getBoundingClientRect();
-//     let stickyInfo = stickyWrapper.getBoundingClientRect();
-//     if (containerInfo.top > stickyInfo.top) {
-//         return true;
-//     }
-//     if (containerInfo.left > stickyInfo.left) {
-//         return true;
-//     }
-//     if (containerInfo.bottom < stickyInfo.bottom) {
-//         return true;
-//     }
-//     if (containerInfo.right < stickyInfo.right) {
-//         return true;
-//     }
-// }
+        let moveY = event.changedTouches[0].clientY - posY;
+        let moveX = event.changedTouches[0].clientX - posX;
 
-function onDrag() {
+        // console.log(moveX, moveY);
+        stickyWrapper.style.top = moveY + 'px';
+        stickyWrapper.style.left = moveX + 'px';
+    });
+    stickyContainer.addEventListener('touchmove', (event) => {
+        if (!isTouched) return;
 
-}
+        let moveY = event.changedTouches[0].clientY - posY;
+        let moveX = event.changedTouches[0].clientX - posX;
+        stickyWrapper.style.top = moveY + 'px';
+        stickyWrapper.style.left = moveX + 'px';
+    });
+    stickyWrapper.addEventListener('touchend', () => {
+        isTouched = false;
+    })
+    stickyWrapper.addEventListener('drag', () => false)
+});
