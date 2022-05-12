96f39967dcf9a3eac8a1e044846ccf9d4d4baf22
Vicky Gupta <vickyguptaa7@gmail.com>
2022-05-13 00:15:54 +0530

    added darkMode

--- src/js/smallEvents.js
+++ src/js/smallEvents.js
@@ -4,7 +4,10 @@ const penTools = document.querySelector('.pen-tool');
 const penToolsWrapper = document.querySelector('.pen-tools-wrapper');
 const dropMenuBtn = document.querySelector('.drop-menu');
 const dropMenuItemsWrapper = document.querySelector('.drop-menu-items-container');
+const switchThemeBtn = document.querySelector('.switch-theme');
+const body = document.querySelector('body');
 
+console.log(switchThemeBtn);
 
 console.log(bucketColorWrapper, bucketSet);
 
@@ -21,10 +24,21 @@ penTools.addEventListener('click', () => {
     penToolsWrapper.classList.toggle('-translate-x-[70%]');
 })
 
-dropMenuBtn.addEventListener('click',()=>{
+dropMenuBtn.addEventListener('click', () => {
     dropMenuBtn.firstElementChild.classList.toggle('-rotate-90');
     dropMenuItemsWrapper.classList.toggle('translate-x-[45%]');
     dropMenuItemsWrapper.classList.toggle('-translate-y-[70%]');
     dropMenuItemsWrapper.classList.toggle('scale-0');
+})
 
+switchThemeBtn.addEventListener('click', () => {
+    if (switchThemeBtn.firstElementChild.classList.contains('fa-moon')) {
+        switchThemeBtn.innerHTML = `<i class="fa-solid fa-sun text-[1.5rem]"></i>`;
+    }
+    else {
+        switchThemeBtn.innerHTML = `<i class="fa-solid fa-moon text-[1.5rem] "></i>`;
+    }
+    body.classList.toggle('darkMode');
+    // <!-- <i class="fa-solid fa-moon text-[1.5rem] "></i> -->
+    // <i class="fa-solid fa-sun text-[1.5rem]"></i>
 })
