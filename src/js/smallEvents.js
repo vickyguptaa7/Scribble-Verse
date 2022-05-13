2ac8464094296ac9edc72f9598ccc8fdbdc70b42
Vicky Gupta <vickyguptaa7@gmail.com>
2022-05-14 00:14:16 +0530

    added comments to smallEvents js file

--- src/js/smallEvents.js
+++ src/js/smallEvents.js
@@ -9,34 +9,51 @@ const body = document.querySelector('body');
 const toolContainer = document.querySelector('.tools-container');
 const hideUnhideBtn = document.querySelector('.hide-unhide');
 
-let isDarkMode = localStorage.getItem('isDarkMode');
+let isDarkMode = false;
 
-if (isDarkMode === null)
-    isDarkMode = false;
-else if (isDarkMode === 'true')
-    isDarkMode = true;
-else
-    isDarkMode = false;
+// Local Storage To Get The Prev Stored Result;
+function storePrevMode() {
+    isDarkMode = localStorage.getItem('isDarkMode');
 
+    // it store data in string form so converted back to bool form
+    if (isDarkMode === null)
+        isDarkMode = false;
+    else if (isDarkMode === 'true')
+        isDarkMode = true;
+    else
+        isDarkMode = false;
+
+    if (isDarkMode == true) {
+        switchTheme();
+        isDarkMode = true;
+        localStorage.setItem('isDarkMode', isDarkMode);
+    }
+}
+
+storePrevMode();
+
+// Show The Bucket Color For The Background Change
 bucketSet.addEventListener('click', () => {
     bucketColorWrapper.classList.toggle('invisible');
     bucketColorWrapper.classList.toggle('scale-0');
     bucketColorWrapper.classList.toggle('-translate-x-[100%]');
 })
 
-
+// Show The Pen Tools Color And Size
 penTools.addEventListener('click', () => {
     penToolsWrapper.classList.toggle('invisible');
     penToolsWrapper.classList.toggle('scale-0');
     penToolsWrapper.classList.toggle('-translate-x-[70%]');
 })
 
+// Drop Menu Button For The Top Right For Local Storage Options
 dropMenuBtn.addEventListener('click', () => {
     dropMenuBtn.firstElementChild.classList.toggle('-rotate-90');
     dropMenuItemsWrapper.classList.toggle('translate-x-[45%]');
     dropMenuItemsWrapper.classList.toggle('-translate-y-[70%]');
     dropMenuItemsWrapper.classList.toggle('scale-0');
 })
+
 
 switchThemeBtn.addEventListener('click', switchTheme)
 function switchTheme() {
@@ -51,10 +68,9 @@ function switchTheme() {
 
     localStorage.setItem('isDarkMode', isDarkMode);
     body.classList.toggle('darkMode');
-    // <!-- <i class="fa-solid fa-moon text-[1.5rem] "></i> -->
-    // <i class="fa-solid fa-sun text-[1.5rem]"></i>
 }
 
+// Hide And Unhide The Left Side Tool Bar
 hideUnhideBtn.addEventListener('click', () => {
     toolContainer.classList.toggle('left-[1.5rem]');
     toolContainer.classList.toggle('left-[-6rem]');
@@ -62,8 +78,3 @@ hideUnhideBtn.addEventListener('click', () => {
     hideUnhideBtn.firstElementChild.classList.toggle('fa-angles-right');
 })
 
-if (isDarkMode == true) {
-    switchTheme();
-    isDarkMode = true;
-    localStorage.setItem('isDarkMode', isDarkMode);
-}
